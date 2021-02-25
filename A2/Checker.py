77eb1296e08e007d245d0944476017cda7f963ca
Apar Ahuja <66245948+AparAhuja@users.noreply.github.com>
2021-02-25 10:58:14 +0530

    Add files via upload

---
+++ A2/Checker.py
@@ -0,0 +1,25 @@
+QTSPIM_OUTPUT = "MIPS_output.txt"
+no_of_cases = 1
+
+f = open(QTSPIM_OUTPUT, "r")
+g = open("Expected_output.txt", "r")
+k = open("Difference.txt", "w")
+
+cnt = 0
+for i in range(no_of_cases):
+    str1 = f.readline()
+    str2 = g.readline()
+    a1 = str1[:-1]
+    a2 = str2[:-1]
+    error = (a1-a2)
+
+    if(error == 0 ):
+        k.write("0\n")
+        cnt+=1
+    else:
+        k.write(str(error)+"\n")
+k.write("Accuracy is " + str(cnt/no_of_cases*100) + "%\n")
+
+k.close()
+f.close()
+g.close()
