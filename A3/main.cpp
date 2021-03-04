8ce7d56bed43e9b336fee5d38f273b9abea0acf4
Apar Ahuja <66245948+AparAhuja@users.noreply.github.com>
2021-03-04 18:56:27 +0530

    Update main.cpp

--- A3/main.cpp
+++ A3/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 using namespace std;
-class register_file{
-    
+
+string file_path = "/Users/aparahuja/Desktop/IITD/COL216 - Arch/COL216_Lab/A3/A3/input.txt";
+struct register_file{
+    //INSTRUCTION COUNT
+    //int ins_cnt_add, ins_cnt_sub...
+    //int ins_cnt[10] ... add is 0, sub is 1, etc ...
+    //map<string, int> ins_cnt;
+    int cycle_cnt = 0;
+    void print_reg_file(){}
+    void execution_stats(){}
+    void add(){cycle_cnt++;}
+    void sub(){cycle_cnt++;}
+    void mul(){cycle_cnt++;}
+    void beq(){cycle_cnt++;}
+    void bne(){cycle_cnt++;}
+    void slt(){cycle_cnt++;}
+    void j(){cycle_cnt++;}
+    void lw(){cycle_cnt++;}
+    void sw(){cycle_cnt++;}
+    void addi(){cycle_cnt++;}
 };
 
 //add line to memory
@@ -11,7 +30,7 @@ void addToMemory(string line){
     cout<<line<<"\n"; // for debugging
 }
 int main(int argc, const char * argv[]) {
-    ifstream infile("/Users/aparahuja/Desktop/IITD/COL216 - Arch/COL216_Lab/A3/A3/input.txt");
+    ifstream infile(file_path);
     //File error
     if(!infile.is_open()){
         cout<<"ERROR: Unable to open file.\n";
